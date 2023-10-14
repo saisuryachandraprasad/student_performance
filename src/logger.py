ebf8c0ab0f5484815b5508fea00f6b7ed099a583
saisuryachandraprasad <saisuryachandraprasad@gmail.com>
2023-10-14 09:02:57 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+""" LOG FILE FORMAT """
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+""" PATH WHRE LOGS WILL BE CREATED """
+log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format= "[%(asctime)s] %(lineno)s %(name)s -- %(levelname)s- %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info('logging test')
